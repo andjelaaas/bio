d6cc5ee75818b65f31f800024a429e82293364ce
Andjela Stanic <andjela98stanic@gmail.com>
2019-01-14 20:35:40 +0100

    aa
    a

--- js/main.js
+++ js/main.js
@@ -24,8 +24,3 @@
 	});
 	//when dom is ready end
 })(jQuery);
-
-$( 'bs-example-navbar-collapse-1 .navbar-nav a' ).on( 'click', function () {
-    $( 'bs-example-navbar-collapse-1 .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
-    $( this ).parent( 'li' ).addClass( 'active' );
-});
